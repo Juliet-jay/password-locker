d59d13fe1c34f1b2b4b93a8ae998285d108a21e8
kathure <juliekmuthee@gmail.com>
2019-07-19 18:42:54 +0300

    added a teardowm method that does clean up after test case has run

--- user.py
+++ user.py
@@ -1,6 +1,50 @@
 class User:
+    
     '''
     class that generates a new instance of a password user
-    '''
+    __init__ method that helps us to define properitis for our objet
     
-    def __init__(self,name)
+    Args:
+    '''
+    user_list = [] #Empty user list
+    
+    def __init__(self,name,password):
+        
+        self.name=name
+        self.password=password
+        
+    def save_user(self):
+        
+        
+        '''
+        save_contact method save user objects into user_list
+        '''
+        User.user_list.append(self)
+    @classmethod
+    def user_verified(cls,name,password):
+    
+        '''
+        methods that takes the user logings and returs boolean its true
+        
+        Args:
+            name:User name to search
+            password:password to match
+            
+        return:
+              Boolean true if they both match to a user and false 
+              if it doesn't
+        '''
+        for user in cls.user_list:
+            if user.name==name and user.password==password:
+                return True
+            return False
+          
+        
+   
+    
+  
+        
+
+        
+        
+        
